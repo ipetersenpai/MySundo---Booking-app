f49f288b6c41d88eac934122cf51401a56d9869f
Laurence Troy Valdez <laurencetroyv@gmail.com>
2023-01-20 02:42:07 +0800

    User information (Create Account First Screen)

---
+++ src/features/Accounts/screens/UserInformationStyles.js
@@ -0,0 +1,157 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+const windowHeight = Dimensions.get("window").height;
+
+export const styles = StyleSheet.create({
+  containter: {
+    flex: 1,
+    backgroundColor: "#FDFDFD",
+  },
+
+  pannel1: {
+    position: "absolute",
+    alignSelf: "center",
+    width: "90%",
+    height: windowHeight / 1.2,
+    alignItems: "center",
+    borderRadius: 15,
+    top: "5%",
+  },
+
+  logostyle: {
+    top: 42,
+    position: "absolute",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+
+  inputfield: {
+    height: windowHeight / 20,
+    width: "85%",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+    paddingStart: 15,
+    paddingEnd: 15,
+    fontSize: RFValue(14, 680),
+    fontFamily: "Roboto",
+  },
+
+  inputstyle: {
+    alignItems: "center",
+    width: "100%",
+    marginBottom: "5%",
+  },
+  miniplaceholder: {
+    position: "absolute",
+    fontSize: RFValue(10, 680),
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    left: "11%",
+    top: "-12%",
+    backgroundColor: "white",
+  },
+
+  miniplaceholder1: {
+    position: "absolute",
+    fontSize: RFValue(10, 680),
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    left: "3%",
+    top: "-12%",
+    backgroundColor: "white",
+  },
+
+  dropdownmenu: {
+    position: "absolute",
+    width: "7%",
+    height: "40%",
+    alignSelf: "flex-end",
+    right: "5%",
+    top: "30%",
+  },
+
+  dropdownarea: {
+    width: "85%",
+    height: windowHeight / 4,
+    borderRadius: 10,
+    backgroundColor: "white",
+    top: "2%",
+    elevation: 5,
+  },
+
+  searchinput: {
+    height: windowHeight / 23,
+    width: "90%",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    paddingStart: 10,
+    paddingEnd: 35,
+    fontSize: RFValue(12, 680),
+    marginTop: 15,
+    fontFamily: "Roboto",
+    alignSelf: "center",
+  },
+
+  renderItem: {
+    width: "80%",
+    height: 50,
+    borderBottomWidth: 0.2,
+    borderBottomColor: "#8e8e8e",
+    alignSelf: "center",
+  },
+
+  fontstyle2: {
+    top: 20,
+    fontSize: RFValue(12, 680),
+  },
+
+  bdayandgenderinputstyle: {
+    height: windowHeight / 20,
+    width: "47%",
+    borderColor: "black",
+    paddingStart: 15,
+    paddingEnd: 15,
+    fontSize: RFValue(14, 680),
+    fontFamily: "Roboto",
+    marginRight: "5%",
+  },
+
+  container3: {
+    width: "85%",
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: "5%",
+  },
+  datefont: {
+    fontSize: RFValue(14, 680),
+    fontFamily: "Roboto",
+    top: "23%",
+  },
+
+  loginfont: {
+    textAlign: "center",
+    top: "-60%",
+    fontSize: RFValue(22, 680),
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  Loginbutton: {
+    width: "85%",
+    height: "11%",
+    top: "5%",
+    borderRadius: 10,
+  },
+
+  sample: {
+    width: "100%",
+    height: "70%",
+    borderRadius: 10,
+  },
+});
