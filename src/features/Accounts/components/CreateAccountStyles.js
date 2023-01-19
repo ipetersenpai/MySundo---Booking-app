ea46256f8bf2acc76b73d3c2e86e74aa4401d0b5
Laurence Troy Valdez <laurencetroyv@gmail.com>
2023-01-20 02:41:43 +0800

    Create Account

---
+++ src/features/Accounts/components/CreateAccountStyles.js
@@ -0,0 +1,165 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+  },
+
+  bgstyle: {
+    width: windowWidth,
+    height: windowHeight,
+  },
+
+  backbutton: {
+    position: "absolute",
+    paddingLeft: 10,
+    paddingTop: 40,
+    justifyContent: "flex-start",
+    top: "-1%",
+  },
+
+  pannel1: {
+    flex: 1,
+    position: "absolute",
+    alignItems: "center",
+    backgroundColor: "white",
+    width: "90%",
+    height: windowHeight / 1.8,
+    alignSelf: "center",
+    top: "23%",
+    borderRadius: 20,
+    shadowRadius: 1,
+    elevation: 5,
+  },
+  fontstyle1: {
+    textAlign: "center",
+    fontSize: RFValue(20, 680),
+    fontWeight: "700",
+    fontFamily: "Roboto",
+    paddingTop: 30,
+  },
+
+  fontstyle3: {
+    textAlign: "center",
+    alignSelf: "center",
+    fontSize: RFValue(14, 680),
+    fontFamily: "Roboto",
+    backgroundColor: "white",
+    width: windowWidth / 3,
+  },
+
+  SignUpbutton: {
+    alignSelf: "center",
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "#25FFC4",
+    width: 160,
+    height: 40,
+    top: 50,
+    marginBottom: "5%",
+    borderRadius: 10,
+  },
+
+  Loginbutton: {
+    width: windowWidth / 2,
+    height: "11%",
+    top: "2%",
+    borderRadius: 10,
+  },
+
+  sample: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
+
+  loginfont: {
+    textAlign: "center",
+    top: "-85%",
+    fontSize: RFValue(20, 680),
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  line: {
+    top: "3%",
+    width: "80%",
+    borderColor: "#d9d9d9",
+    borderWidth: 1,
+    marginLeft: "10%",
+    marginRight: "10%",
+  },
+
+  container2: {
+    position: "absolute",
+    height: windowHeight / 14,
+    top: windowHeight / 2.34,
+    width: "70%",
+    alignItems: "stretch",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+
+  icon1: {
+    height: "100%",
+    width: "25%",
+  },
+
+  icon2: {
+    height: "100%",
+    width: "25%",
+  },
+
+  icon3: {
+    height: "100%",
+    width: "25%",
+  },
+
+  test: {
+    marginTop: "5%",
+    width: "90%",
+    height: windowHeight / 5,
+    backgroundColor: "white",
+  },
+
+  textinputstyle: {
+    alignSelf: "center",
+    height: windowHeight / 20,
+    width: "80%",
+    borderColor: "#d9d9d9",
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: "5%",
+    paddingStart: 10,
+    paddingEnd: 10,
+    fontSize: RFValue(12, 680),
+  },
+
+  fontstyle4: {
+    fontSize: RFValue(12, 680),
+    fontFamily: "Roboto",
+  },
+
+  clickherestyle: {
+    fontSize: RFValue(12, 680),
+    fontFamily: "Roboto",
+    color: "#438EFF",
+    fontWeight: "500",
+  },
+
+  eyestyle: {
+    width: windowWidth / 15,
+    height: windowHeight / 30,
+  },
+
+  signuphere_nav: {
+    top: windowHeight / 7,
+    flexDirection: "row",
+  },
+});
---
+++ src/features/Accounts/screens/CreateAccount.js
@@ -0,0 +1,214 @@
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StatusBar,
+  TextInput,
+} from "react-native";
+import { React, useState } from "react";
+import { styles } from "../components/CreateAccountStyles";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
+
+export default function CreateAccount() {
+  const navigation = useNavigation();
+  const [passoption, setPassoption] = useState(false);
+  const [showpass, setShowpass] = useState(true);
+  const [passoption1, setPassoption1] = useState(false);
+  const [showpass1, setShowpass1] = useState(true);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+
+  const [isFocused, setIsFocused] = useState({
+    entry1: false,
+    entry2: false,
+    entry3: false,
+  });
+
+  const handleInputFocus = (textinput) => {
+    setIsFocused({
+      [textinput]: true,
+    });
+  };
+  const handleInputBlur = (textinput) => {
+    setIsFocused({
+      [textinput]: false,
+    });
+  };
+
+  let userinputed = { email: email, password: password };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <Image
+        style={styles.bgstyle}
+        source={require("../../../../assets/background3.jpg")}
+        resizeMode="cover"
+      />
+
+      <View style={styles.backbutton}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("FirstScreen");
+          }}
+          style={{ top: 5 }}>
+          <Icon name={"arrow-left"} size={42} color={"black"} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.pannel1}>
+        <Text style={styles.fontstyle1}>CREATE NEW ACCOUNT</Text>
+
+        <View style={styles.test}>
+          {/* kani na part ang input text */}
+          <TextInput
+            style={
+              isFocused.entry1
+                ? [styles.textinputstyle, { borderColor: "#438EFF" }]
+                : styles.textinputstyle
+            }
+            placeholder="Email"
+            onChangeText={setEmail}
+            onFocus={() => handleInputFocus("entry1")}
+            onBlur={() => handleInputBlur("entry1")}
+          />
+
+          <TextInput
+            style={
+              isFocused.entry2
+                ? [styles.textinputstyle, { borderColor: "#438EFF" }]
+                : styles.textinputstyle
+            }
+            placeholder="Password"
+            onChangeText={setPassword}
+            secureTextEntry={showpass}
+            onFocus={() => handleInputFocus("entry2")}
+            onBlur={() => handleInputBlur("entry2")}
+          />
+
+          <TextInput
+            style={
+              isFocused.entry3
+                ? [styles.textinputstyle, { borderColor: "#438EFF" }]
+                : styles.textinputstyle
+            }
+            placeholder="Re-enter Password"
+            onChangeText={setPassword1}
+            secureTextEntry={showpass1}
+            onFocus={() => handleInputFocus("entry3")}
+            onBlur={() => handleInputBlur("entry3")}
+          />
+
+          <TouchableOpacity
+            style={{ top: "-32%", left: "77%" }}
+            onPress={() => {
+              setShowpass1(!showpass1);
+              setPassoption1(!passoption1);
+            }}>
+            <Image
+              style={styles.eyestyle}
+              source={
+                passoption1 === false
+                  ? require("../../../../assets/eye_open.png")
+                  : require("../../../../assets/eye_close.png")
+              }
+              resizeMode="cover"
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{ top: "-82%", left: "77%" }}
+            onPress={() => {
+              setShowpass(!showpass);
+              setPassoption(!passoption);
+            }}>
+            <Image
+              style={styles.eyestyle}
+              source={
+                passoption === false
+                  ? require("../../../../assets/eye_open.png")
+                  : require("../../../../assets/eye_close.png")
+              }
+              resizeMode="cover"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={styles.Loginbutton}
+          onPress={() => {
+            // axios({
+            //     method: 'post',
+            //     url: 'http://192.168.1.63:80/api/auth.php',
+            //     data: {
+            //         //user_id:'0005',
+            //         email:'jordan@idolo.com',
+            //         password: 'jordanidolo',
+            //         //first_name: 'michael jordan',
+
+            //     }
+            //   });
+            navigation.push("SUscreen2", userinputed);
+          }}>
+          <Image
+            source={require("../../../../assets/button.png")}
+            resizeMode="cover"
+            style={styles.sample}
+          />
+
+          <Text style={styles.loginfont}>SIGN UP</Text>
+        </TouchableOpacity>
+
+        <View style={styles.signuphere_nav}>
+          <Text style={styles.fontstyle4}>Already have an account?</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("LoginScreen");
+            }}
+            style={{ alignContent: "center" }}>
+            <Text style={styles.clickherestyle}> Login here</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.line} />
+        <Text style={styles.fontstyle3}>or connect using</Text>
+
+        <View style={styles.container2}>
+          <TouchableOpacity style={styles.icon1}>
+            <Image
+              source={require("../../../../assets/fb_logo.png")}
+              resizeMode="cover"
+              style={{ width: "70%", height: "85%" }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.icon2}>
+            <Image
+              source={require("../../../../assets/email_logo.png")}
+              resizeMode="contain"
+              style={{ width: "90%", height: "90%" }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.icon3}>
+            <Image
+              source={require("../../../../assets/in_logo.png")}
+              resizeMode="contain"
+              style={{ width: "90%", height: "90%" }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
