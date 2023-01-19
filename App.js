d9770ad51eee5d442afdf86d6c211e0b412c5d5d
Laurence Troy Valdez <laurencetroyv@gmail.com>
2023-01-20 01:43:32 +0800

    added StatusBar

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import "react-native-gesture-handler";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import NavigationIndex from "./src/infastructure/navigation/NavigationIndex";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationIndex />
+      <StatusBar style="auto" />
     </SafeAreaProvider>
   );
 }
