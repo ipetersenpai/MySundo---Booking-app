fc6f0cef6e54171b5ba1ff73e3434016c0d6c605
Laurence Troy Valdez <laurencetroyv@gmail.com>
2023-01-20 02:32:16 +0800

    Login

---
+++ src/features/Accounts/screens/Login.js
@@ -0,0 +1,225 @@
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StatusBar,
+  TextInput,
+  Alert,
+} from "react-native";
+import { React, useState } from "react";
+import { styles } from "../components/LoginStyles";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import CarouselComponent from "../../../components/CarouselComponents";
+import axios from "../../../plugin/axios";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Login() {
+  const navigation = useNavigation();
+  const [passoption, setPassoption] = useState(false);
+  const [showpass, setShowpass] = useState(true);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isFocused, setIsFocused] = useState({
+    entry1: false,
+    entry2: false,
+  });
+
+  const handleInputFocus = (textinput) => {
+    setIsFocused({
+      [textinput]: true,
+    });
+  };
+  const handleInputBlur = (textinput) => {
+    setIsFocused({
+      [textinput]: false,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <Image
+        style={styles.bgstyle}
+        source={require("../../../../assets/background.jpg")}
+        resizeMode="cover"
+      />
+
+      <View style={styles.carousel}>
+        <CarouselComponent />
+      </View>
+
+      <View style={styles.backbutton}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+          style={{ top: 5 }}>
+          <Icon name={"arrow-left"} size={42} color={"white"} />
+        </TouchableOpacity>
+
+        <Text style={styles.Logo}>
+          MY<Text style={{ color: "#ACFF43" }}>SUNDO</Text>
+        </Text>
+      </View>
+
+      <View style={styles.pannel1}>
+        <Text style={styles.fontstyle1}>LOGIN YOUR ACCOUNT</Text>
+
+        <View style={styles.test}>
+          {/* kani na part ang input text */}
+          <TextInput
+            style={
+              isFocused.entry1
+                ? [styles.textinputstyle, { borderColor: "#438EFF" }]
+                : styles.textinputstyle
+            }
+            placeholder="Email"
+            onChangeText={setEmail}
+            onFocus={() => handleInputFocus("entry1")}
+            onBlur={() => handleInputBlur("entry1")}
+          />
+
+          <TextInput
+            style={
+              isFocused.entry2
+                ? [styles.textinputstyle, { borderColor: "#438EFF" }]
+                : styles.textinputstyle
+            }
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={showpass}
+            onFocus={() => handleInputFocus("entry2")}
+            onBlur={() => handleInputBlur("entry2")}
+          />
+
+          <TouchableOpacity
+            style={{ top: "-32%", left: "77%" }}
+            onPress={() => {
+              setShowpass(!showpass);
+              setPassoption(!passoption);
+            }}>
+            <Image
+              style={styles.eyestyle}
+              source={
+                passoption === false
+                  ? require("../../../../assets/eye_open.png")
+                  : require("../../../../assets/eye_close.png")
+              }
+              resizeMode="cover"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={styles.Loginbutton}
+          onPress={() => {
+            axios
+              .post("/api/auth.php", {
+                email: email,
+                password: password,
+              })
+              .then(function (response) {
+                if (response.data[0].Message === "wrong password") {
+                  Alert.alert(
+                    "Login Failed",
+                    "Your email or password is incorrect Please try again.",
+                    [
+                      {
+                        text: "close",
+                        onPress: () => console.log("alert closed"),
+                      },
+                    ]
+                  );
+                } else if (response.data[0].Message === "No account yet") {
+                  Alert.alert(
+                    "No Account Registered",
+                    "The credential you entered is not found in our system!",
+                    [
+                      {
+                        text: "close",
+                        onPress: () => console.log("alert closed"),
+                      },
+                    ]
+                  );
+                } else if (response.data[0].Message === "Success") {
+                  navigation.navigate("Dashboard");
+                }
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }}>
+          <Image
+            source={require("../../../../assets/button.png")}
+            resizeMode="cover"
+            style={styles.sample}
+          />
+
+          <Text style={styles.loginfont}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <View style={{ top: "-2%", flexDirection: "row" }}>
+          <Text style={styles.fontstyle4}>Forgot the password?</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Screen1");
+            }}
+            style={{ alignContent: "center" }}>
+            <Text style={styles.clickherestyle}> click here</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.signuphere_nav}>
+          <Text style={styles.fontstyle4}>Don’t have an account yet?</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("SUscreen1");
+            }}
+            style={{ alignContent: "center" }}>
+            <Text style={styles.clickherestyle}> Signup here</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.line} />
+        <Text style={styles.fontstyle3}>or connect using</Text>
+
+        <View style={styles.container2}>
+          <TouchableOpacity style={styles.icon1}>
+            <Image
+              source={require("../../../../assets/fb_logo.png")}
+              resizeMode="cover"
+              style={{ width: "70%", height: "85%" }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.icon2}>
+            <Image
+              source={require("../../../../assets/email_logo.png")}
+              resizeMode="contain"
+              style={{ width: "90%", height: "90%" }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.icon3}>
+            <Image
+              source={require("../../../../assets/in_logo.png")}
+              resizeMode="contain"
+              style={{ width: "90%", height: "90%" }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
